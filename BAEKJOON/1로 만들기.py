6349a196774eaa8a41fa7f315f3ff681c3b2b90c
김준휘 <jhkim@gimjunhwiui-MacBookPro.local>
2024-08-26 13:37:14 +0900

    백준 1로 만들기.py

---
+++ BAEKJOON/1로 만들기.py
@@ -0,0 +1,12 @@
+num = int(input())
+
+num_list = [0] * (num+1)
+
+for i in range(2, num+1):
+    num_list[i] = num_list[i-1] + 1
+    if i % 2 == 0:
+        num_list[i] = min(num_list[i], num_list[i//2]+1)
+    if i % 3 == 0:
+        num_list[i] = min(num_list[i], num_list[i//3]+1)
+
+print(num_list[num])
